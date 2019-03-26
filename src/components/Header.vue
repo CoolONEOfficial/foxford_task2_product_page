8974d08828d0eeb57d45e58d11594ab296944d0d
coolone <coolone.official@gmail.com>
2019-03-26 12:56:50 +0300

    Added search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
                 <v-flex xs1>
                     <a id="logo">sears</a>
                 </v-flex>
-                <v-flex xs7>
+                <v-flex xs5>
                     <v-layout column>
 
                         <v-layout row pt-3>
@@ -13,14 +13,16 @@
                             <a>sdfsdf</a>
                         </v-layout>
 
-                        <v-text-field class="pa-0 ma-0"
-                                      color="white"
-                                      solo
-                                      style="max-height: 30px;"
-
-                        ></v-text-field>
+                        <div class="password-wrapper">
+                            <input id="password-field" type="password" class="input" name="password">
+                            <div class="icon-wrapper">
+                                <span class="ion ion-search field-icon toggle-password"></span>
+                            </div>
+                        </div>
 
                     </v-layout>
+                </v-flex>
+                <v-flex xs2>
                 </v-flex>
             </v-layout>
         </div>
@@ -70,4 +72,60 @@
         height: 3em;
     }
 
+    .bg-transparent {
+        background-color: transparent !important;
+    }
+
+    @mixin size($w, $h) {
+        width: $w;
+        height: $h;
+    }
+
+    @mixin font-size($sizeValue: 1.6) {
+        font-size: $sizeValue * 10 + px;
+        font-size: $sizeValue + rem;
+    }
+
+    // styles
+    body {
+        background-color: $primary;
+        .password-wrapper {
+            width: 80%;
+            border-radius: 5px;
+            background-color: white;
+            overflow: hidden;
+            .input {
+                border: none;
+                font: small-caption;
+                @include font-size(1.4);
+                width: calc(100% - 50px);
+                color: $secondary;
+                outline: none;
+                line-height: 1.8;
+                padding-left: 15px;
+            }
+            .icon-wrapper {
+                position: relative;
+                display: inline;
+                float: right;
+                @include size(42px, 36px);
+                background-color: $light;
+                transition: background-color 0.25s ease-out;
+                cursor: pointer;
+                .ion-search {
+                    @include font-size(2.0);
+                    position: absolute;
+                    top: 4px;
+                    right: 10px;
+                    color: #cccccc;
+                    transition: color 0.25s ease-out;
+                }
+                &:hover {
+                    transition: background-color 0.25s ease-in;
+                    background-color: $secondary;
+
+                }
+            }
+        }
+    }
 </style>
